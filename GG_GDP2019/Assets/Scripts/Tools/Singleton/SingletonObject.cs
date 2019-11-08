72d4f78e9af86141bad27adb35149bbf9f4acb46
Pierre CAMILLI <camillipierre@gmail.com>
2019-11-08 22:02:35 +0100

    Add folders + utility tools

---
+++ GG_GDP2019/Assets/Scripts/Tools/Singleton/SingletonObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Singleton base class; provides a static accessor to Components that are only ever in one instance.
+/// If marked as DontDestroyOnLoad in the inspector, the root gameObject is not destroyed when loading new scenes.
+/// </summary>
+public abstract class SingletonObject<T> : MonoBehaviour where T : SingletonObject<T>
+{
+    /// <summary>
+    /// Static singleton instance of the component.
+    /// </summary>
+    static T m_Instance = null;
+    public static T Instance {
+        get {
+            if (!m_Instance)
+            {
+                m_Instance = Resources.FindObjectsOfTypeAll<T>().FirstOrDefault();
+            }
+            return m_Instance;
+        }
+    }
+}
