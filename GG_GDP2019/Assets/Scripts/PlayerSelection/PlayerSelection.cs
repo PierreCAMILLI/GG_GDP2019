aada2a9eff6cc55197760388ecba1c9a0e004d7f
Fax <h4moze4ipzmo6gku4x4eruwsufzs4prbcdwgc5ufxktgnoaxblfq>
2019-11-09 15:10:49 +0100

    Debut ui

---
+++ GG_GDP2019/Assets/Scripts/PlayerSelection/PlayerSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelection : MonoBehaviour
+{
+
+    public int Number = 0;
+    public bool Selected = false;
+    public bool Connected = false;
+    public int SelectedObject = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO : if(Controls.Instance.LeftDown())
+
+    }
+}
---
+++ GG_GDP2019/Assets/Scripts/PlayerSelection/PlayersSelectionManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayersSelectionManager : MonoBehaviour
+{
+
+    private GameObject[] playersSelection;
+    public RectTransform FirstPanel;
+    public PlayerSelection PrefabPanel;
+    private float SpaceBetweenTwoPanels;
+    private float posXInitial, posYInitial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posXInitial = FirstPanel.transform.position.x;
+        posYInitial = FirstPanel.transform.position.y;
+
+        SpaceBetweenTwoPanels = ((1920 - 2*posXInitial)-4*(FirstPanel.rect.width * FirstPanel.localScale.x))/4;
+        playersSelection = new GameObject[4];
+        for (int i = 0; i < 4; i++)
+        {
+            PlayerSelection playerSelec = FirstPanel.GetComponent<PlayerSelection>();
+            if (i != 0)
+                playerSelec = Instantiate(PrefabPanel, FirstPanel.parent.transform);
+
+            playerSelec.Number = i;
+            playersSelection[i] = playerSelec.gameObject;
+
+            playerSelec.transform.SetPositionAndRotation(new Vector3(posXInitial + i * ((FirstPanel.rect.width*FirstPanel.localScale.x)+ SpaceBetweenTwoPanels), posYInitial, 0), Quaternion.identity);
+            playerSelec.GetComponent<Image>().color = CommonProperties.Instance._colors[i];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
