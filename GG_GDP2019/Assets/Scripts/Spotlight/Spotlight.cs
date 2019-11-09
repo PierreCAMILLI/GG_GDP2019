c32a42fbcc12e12fc9ce7e8fcaaf7eb2a8e571e3
bonnelc <bonnelc@DESKTOP-2VJF3G5.localdomain>
2019-11-09 14:44:57 +0100

    spotlight

---
+++ GG_GDP2019/Assets/Scripts/Spotlight/Spotlight.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spotlight : MonoBehaviour
+{
+    public int numberDetectedWaste = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Move is called in the update method of SpotlightController 
+    public void Move(float dx, float dz, float speed, Vector2Int direction)
+    {
+        float newdx = direction[0] * dx * speed * Time.deltaTime;
+        float newdz = direction[1] * dz * speed * Time.deltaTime;
+        transform.Translate(new Vector3(newdx, 0, newdz));
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<>() != null )
+        {
+            numberDetectedWaste++;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<>() != null)
+        {
+            numberDetectedWaste--;
+        }
+    }
+}
