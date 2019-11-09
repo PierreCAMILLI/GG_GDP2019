c7a7f9ce8fa12ca7077c66f68ae967e8ad4f7c5f
Pierre CAMILLI <camillipierre@gmail.com>
2019-11-09 16:15:39 +0100

    Implémentation de la caméra

---
+++ GG_GDP2019/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private TransformGrouper _grouper;
+
+    [SerializeField]
+    private Bounds1D _fieldOfView;
+
+    [SerializeField]
+    private float _zoomLimiter;
+
+    [SerializeField]
+    private float distance;
+
+    private Vector3 _direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _direction = transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = _grouper.transform.position + (-_direction * distance);
+
+        float greatestDistance = _grouper.Bounds.size.x;
+        float newZoom = Mathf.Lerp(_fieldOfView.Min, _fieldOfView.Max, greatestDistance / _zoomLimiter);
+        Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+
+}
---
+++ GG_GDP2019/Assets/Scripts/Camera/TransformGrouper.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformGrouper : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> _transforms;
+
+    private Bounds _bounds;
+    public Bounds Bounds
+    {
+        get { return _bounds; }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        UpdateBounds();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateBounds();
+        transform.position = _bounds.center;
+    }
+
+    private void UpdateBounds()
+    {
+        _bounds = new Bounds();
+
+        if (_transforms.Count > 0)
+        {
+            _bounds = new Bounds(_transforms[0].position, Vector3.zero);
+            for (int i = 1; i < _transforms.Count; ++i)
+            {
+                _bounds.Encapsulate(_transforms[i].position);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        UpdateBounds();
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(_bounds.center, 0.1f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(_bounds.center, _bounds.size);
+    }
+}
