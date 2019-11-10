056751142973b37f9f68ad2cc5a6bce44f619636
bonnelc <bonnelc@DESKTOP-2VJF3G5.localdomain>
2019-11-10 21:55:20 +0100

    début son

---
+++ GG_GDP2019/Assets/Scripts/Menu/PlayerSelection/Weapons/Vacuum/VacuumSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumSound : MonoBehaviour
+{
+    public AudioSource mySound;
+    public bool VacuumUsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VacuumUsed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (VacuumUsed)
+        {
+            mySound.enabled = true;
+            mySound.loop = true;
+        }
+        else
+        {
+            mySound.enabled = false;
+            mySound.loop = false;
+        }
+    }
+}
--- GG_GDP2019/Assets/Scripts/Menu/PlayerSelection/Weapons/Vacuum/VacuumWeapon.cs
+++ GG_GDP2019/Assets/Scripts/Menu/PlayerSelection/Weapons/Vacuum/VacuumWeapon.cs
@@ -9,21 +9,26 @@ public class VacuumWeapon : AbstractWeapon
     [SerializeField]
     private float _power = 5f;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
     public override void OnUseDown(Hero hero)
     {
         Transform vacuum = hero.gameObject.transform.Find("perso2").Find("vacuum").Find("vacuumActivable");
         vacuum.gameObject.SetActive(true);
+        VacuumSound v = vacuum.GetComponent<VacuumSound>();
+        v.VacuumUsed = true;
+
     }
 
     public override void OnUseUp(Hero hero)
     {
         Transform vacuum = hero.gameObject.transform.Find("perso2").Find("vacuum").Find("vacuumActivable");
         vacuum.gameObject.SetActive(false);
+        VacuumSound v = vacuum.GetComponent<VacuumSound>();
+        v.VacuumUsed = false;
     }
 
 
