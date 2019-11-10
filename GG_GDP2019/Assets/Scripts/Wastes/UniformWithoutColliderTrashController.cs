fc51deb2ed7c8ad8921bbd6d9c63160ae3e217b4
Amaury Butaux <amaury.butaux@grenoble-inp.org>
2019-11-10 15:43:45 +0100

    Better physics

---
+++ GG_GDP2019/Assets/Scripts/Wastes/UniformWithoutColliderTrashController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniformWithoutColliderTrashController : MonoBehaviour
+{
+    [SerializeField]
+    private TrashCreator trashCreator;
+    [SerializeField]
+    private Bounds area;
+    [SerializeField]
+    private float trashDensity;
+
+    [SerializeField]
+    private Collider[] colliders;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int trashCount = (int) (4 * area.extents.x * area.extents.z * trashDensity);
+
+        for(int i = 0; i < trashCount; i ++)
+        {
+            while(!trashCreator.CreateTrashOutsideColliders(new Vector3(Random.Range(-area.extents.x, area.extents.x),
+                                                   Random.Range(0, area.extents.y),
+                                                   Random.Range(-area.extents.z, area.extents.z)), colliders));
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
