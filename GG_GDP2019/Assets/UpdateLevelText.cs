6954a5cc5ff266b43fe597c9437180d5767426ea
Amaury Butaux <amaury.butaux@grenoble-inp.org>
2019-11-11 13:04:13 +0100

    autres problèmes

---
+++ GG_GDP2019/Assets/UpdateLevelText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateLevelText : MonoBehaviour
+{
+    [SerializeField]
+    private Text levelText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        levelText.text = "LVL:" + GameManager.Instance.level;
+    }
+}
