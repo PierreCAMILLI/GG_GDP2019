b586ff433a9a1828785e62092824dbbfd2517159
Anton Monjon <monjon.an@gmail.com>
2019-11-11 11:46:49 +0100

    adding a script to move the truck

---
+++ GG_GDP2019/Assets/Scripts/Truck/TruckController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(gameObject.transform.position.z >= 3){
+        //     gameObject.SetActive(false);
+        // }
+        transform.Translate(-Time.deltaTime, 0, 0);
+    }
+}
